9cd2412b76c9a0f25239dd79f797c45760748761
rogerrendons <1584@holbertonschool.com>
2020-03-15 21:19:17 +0000

    creating header and _printf files

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _printf - produce ouput according to a format.
+ * @format: string that specifies how subsequent
+ * arguments are converter for ouput.
+ * Return: total number of characters prinrted.
+ * Or negarive value if an output error or an encoding error.
+*/
+int _printf(const char *format, ...)
+{
+	va_list arguments;
+
+	va_start(arguments, format);
+
+	va_end(arguments);
+}
