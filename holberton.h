5a10f2c33b9fb25c14bb15ba9b58ae97f06305c9
rogerrendons <60365915+rogerrendons@users.noreply.github.com>
2020-03-16 11:12:49 -0500

    Roger (#2)
    adding _putchar and print_number files

--- holberton.h
+++ holberton.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
+int _putchar(char c);
+void print_number(int n);
 
 #endif /* HOLBERTON_H */
