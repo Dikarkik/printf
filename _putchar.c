caea30301f2b0e816cdd8de1e1f1100b7b8f6999
Diana Carolina Quintero <47155627+KaroDev3@users.noreply.github.com>
2020-03-16 18:45:50 -0500

    Adding pointer system to count every _putchar call (#6)

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,23 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * _putchar - writes the character c to stdout.
+ * @c: The character to print.
+ * @p_total: pointer to total_printed.
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char c, int *p_total)
 {
-	return (write(1, &c, 1));
+	int return_write = 0;
+
+	return_write = write(1, &c, 1);
+
+	if (return_write == 1)
+	{
+		*p_total = *p_total + 1;
+		return (1);
+	}
+
+	return (0);
 }
