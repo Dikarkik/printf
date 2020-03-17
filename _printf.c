621e1f23849ca1cc5595ec62c98548ff0253ed74
Diana Carolina Quintero <47155627+KaroDev3@users.noreply.github.com>
2020-03-17 17:30:25 -0500

    fix % and spaces case (#10)

--- _printf.c
+++ _printf.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 
 /**
+ * check_spaces_flag - call _putchar based on a flag.
+ * @p_spaces_flag: the flag.
+ * @c: char.
+ * @p_total: pointer to total_printed.
+ * Return: always void.
+ */
+void check_spaces_flag(int *p_spaces_flag, char c, int *p_total)
+{
+	if (*p_spaces_flag == 1)
+	{
+		*p_spaces_flag = 0;
+		_putchar(' ', p_total);
+	}
+	else
+		_putchar(c, p_total);
+}
+/**
  * _printf - produce ouput according to a format.
  * @format: string that specifies how subsequent.
  * arguments are converter for ouput.
@@ -13,6 +30,8 @@ int _printf(const char *format, ...)
 	int index = 0;
 	int total_printed = 0;
 	int *p_total = &total_printed;
+	int spaces_flag = 0;
+	int *p_spaces_flag = &spaces_flag;
 
 	va_start(arguments, format);
 
@@ -20,8 +39,18 @@ int _printf(const char *format, ...)
 	{
 		if (format[index] == '%')
 		{
-			if (check_type(format[index + 1], arguments, p_total) == 1)
+			if (format[index + 1] == ' ')
+			{
+				spaces_flag = 1;
+
+				while (format[index + 1] == ' ')
 				index++;
+			}
+			if (check_type(format[index + 1], arguments, p_total) == 1)
+			{
+				index++;
+				spaces_flag = 0;
+			}
 			else
 			{
 				_putchar('%', p_total);
@@ -29,7 +58,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			_putchar(format[index], p_total);
+			check_spaces_flag(p_spaces_flag, format[index], p_total);
 		}
 
 		index++;
